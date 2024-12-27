17946b728767f2dec5124c74ead679bfd5f4f4b9
Nourhan Essam <nourhan.essam.makhlouf@gmail.com>
2024-12-27 14:35:48 +0200

    test creating databse and conct it sql server from visual studio

---
+++ Library-System/dbo/Tables/Book.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Book] (
+    [Id]    INT        NOT NULL,
+    [Title] NCHAR (20) NOT NULL,
+    [ISBN] varchar(13) not null,
+);
+
--- Library/Book.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Book]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[Title] varchar not null,
-	[Category_id] int not null,
-
-)
